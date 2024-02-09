22abae3ccc48da956583a1fc8546294143de867b
Arman <144450534+JoeMac12@users.noreply.github.com>
2024-02-09 14:54:56 -0400

    fix

--- Assets/Scripts/projectile.cs
+++ Assets/Scripts/projectile.cs
@@ -6,6 +6,12 @@ public class projectile : MonoBehaviour
 {
     public float speed = 10f;
     public float damage = 10f;
+    public float lifetime = 3f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
 
     void Update()
     {
