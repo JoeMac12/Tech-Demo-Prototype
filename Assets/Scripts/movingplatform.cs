0826b1004058003cf07d38848c8004092e32f66a
Arman <144450534+JoeMac12@users.noreply.github.com>
2024-02-08 22:55:09 -0400

    Create moving platform that moves from 2 points

---
+++ Assets/Scripts/movingplatform.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingplatform : MonoBehaviour
+{
+    public Transform pointA; // Do this so we can set any points we want
+    public Transform pointB;
+    public float speed = 2.0f;
+
+    private Vector3 nextPosition;
+
+    void Start() // Start at A
+    {
+        nextPosition = pointA.position;
+    }
+
+    void Update() // Move the platform from the 2 points
+    {
+        if (Vector3.Distance(transform.position, nextPosition) < 0.1f)
+        {
+            nextPosition = nextPosition == pointA.position ? pointB.position : pointA.position;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, speed * Time.deltaTime);
+    }
+}
